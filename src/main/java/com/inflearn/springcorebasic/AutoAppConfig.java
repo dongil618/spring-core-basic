3910d06824712596d6ba9000ca21dc135ce08074
dongil618 <rlaehddlf618@gmail.com>
2022-11-17 23:17:41 +0900

    중복 등록과 충돌(#2)

--- src/main/java/com/inflearn/springcorebasic/AutoAppConfig.java
+++ src/main/java/com/inflearn/springcorebasic/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.inflearn.springcorebasic;
 
+import com.inflearn.springcorebasic.member.MemberRepository;
+import com.inflearn.springcorebasic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,4 +12,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+//    수동 등록 vs 자동 등록 충돌 테스트
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
